0a853d555f68d8fc420a68cd781fd8cef38adab0
Hakan Seven <hakanseven12@gmail.com>
2021-11-10 17:03:50 +0300

    resources

---
+++ InitGui.py
@@ -0,0 +1,153 @@
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2021 Hakan Seven <hakanseven12@gmail.com>               *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+"""Initialization of the Trails workbench (GUI interface)."""
+
+import FreeCADGui
+
+
+class TrailsWorkbench(FreeCADGui.Workbench):
+    """
+    Class which gets initiated at startup of the GUI.
+    """
+    from libs import icons_path
+
+    MenuText = 'Trails'
+    ToolTip = 'Transportation and Geomatics Engineering Workbench'
+    Icon = icons_path + '/workbench.svg'
+
+    def __init__(self):
+        #dictionary key = name of command / command group.
+        #'gui' - locations in gui where commands are accessed, (summed bitflags)
+        #'cmd' - list of commands to display
+        #'group' - Tuple containing the subgroup description and type.  None/undefined if no group
+
+        self.menu = 1
+        self.toolbar = 2
+        self.context = 4
+        self.group = 8
+
+        self.command_ui = {
+            'Point Tools': {
+                'gui': self.menu + self.toolbar,
+                'cmd': ['Create Cluster',
+                    'Import Points',
+                    'Export Points']},
+
+            'Terrain Tools': {
+                'gui': self.menu + self.toolbar + self.context,
+                'cmd': ['Create Terrain',
+                    'Add Data',
+                    'Terrain Editor']},
+
+            'Section Tools': {
+                'gui': self.menu + self.toolbar,
+                'cmd': ['Create Region',
+                    'Create Sections',
+                    'Compute Areas',
+                    'Create Table']},
+
+            'Pad Tools': {
+                'gui': self.menu + self.toolbar,
+                'cmd': ['Create Pad']},
+
+            'Alignment': {
+                'gui': self.menu + self.toolbar + self.context,
+                'cmd': ['LandXML Importer']},
+
+            'Coordinate System': {
+                'gui': self.toolbar,
+                'cmd': ['Geo Widget']},
+
+            'Add Data': {
+                'gui': self.group,
+                'tooltip': 'Add Data',
+                'type': 'Trails::Terrain',
+                'cmd': ['Import Points',
+                    'Add Cluster']},
+
+            'Terrain Editor': {
+                'gui': self.group,
+                'tooltip': 'Edit Terrain',
+                'type': 'Trails::Terrain',
+                'cmd': ['Add Point',
+                    'Delete Triangle',
+                    'Swap Edge',
+                    'Smooth Terrain']}
+        }
+
+    def GetClassName(self):
+        """
+        Return the workbench classname.
+        """
+        return 'Gui::PythonWorkbench'
+
+    def Initialize(self):
+        """
+        Called when the workbench is first activated.
+        """
+        import DraftTools
+        from libs import CommandGroup
+        from Trails.guitools import gui_cluster, gui_point_importer, gui_point_exporter,\
+            gui_terrain, gui_add_data, gui_terrain_editor, gui_region, gui_sections,\
+            gui_volume, gui_table, gui_pad, gui_landxml_importer, gui_geowidget
+
+        for palette, tool in self.command_ui.items():
+            if tool['gui'] & self.toolbar:
+                self.appendToolbar(palette, tool['cmd'])
+
+            if tool['gui'] & self.menu:
+                self.appendMenu(palette, tool['cmd'])
+
+            if tool['gui'] & self.group:
+                FreeCADGui.addCommand(palette, CommandGroup(
+                    tool['cmd'], tool['tooltip'], tool['type']))
+
+    def Activated(self):
+        """
+        Called when switching to this workbench
+        """
+        if hasattr(FreeCADGui, "Snapper"):
+            FreeCADGui.Snapper.show()
+            import draftutils.init_draft_statusbar as dsb
+            dsb.show_draft_statusbar()
+
+    def Deactivated(self):
+        """
+        Called when switiching away from this workbench
+        """
+        if hasattr(FreeCADGui, "Snapper"):
+            FreeCADGui.Snapper.hide()
+            import draftutils.init_draft_statusbar as dsb
+            dsb.hide_draft_statusbar()
+
+    def ContextMenu(self, recipient):
+        """
+        Right-click menu options
+        """
+        # "recipient" will be either "view" or "tree"
+
+        for _k, _v in self.fn.items():
+            if _v['gui'] & self.context:
+                self.appendContextMenu(_k, _v['cmds'])
+
+FreeCADGui.addWorkbench(TrailsWorkbench())
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Trails
+# FreeCAD Trails Workbench
+Trails is the Transportation and Geomatics Engineering workbench for FreeCAD.
+
+## Features
+
+* Import Survey Data Files
+* Export Point Groups to File
+* Create Surface
+* Edit Surface  
+* Create Regions
+* Create Sections
+* Calculate Volume Areas
+* Create Volume Tables
+* Create Building Pads
+* Import LandXML files.
+
+## Installation
+
+Download and extract the ZIP file into the `.FreeCAD/Mod` folder.
+
+## Screenshots
+
+![](https://community.osarch.org/uploads/editor/n3/gqbzauel9gnz.png "")
+![](https://community.osarch.org/uploads/editor/mq/t3fkzt6q51nr.png "")
+![](https://community.osarch.org/uploads/editor/hn/66wuqmutn9o7.png "")
+![](https://community.osarch.org/uploads/editor/z6/b3hupo69epaw.png "")
+![](https://community.osarch.org/uploads/editor/sv/epkb70fifwg2.png "")
+![](https://community.osarch.org/uploads/editor/8m/y4n02o7kdep4.png "")
+![](https://community.osarch.org/uploads/editor/kp/pxsgaezkvhhg.png "")
+![](https://community.osarch.org/uploads/editor/5g/t7b741rit6fy.gif "")
+![](https://community.osarch.org/uploads/editor/mg/92t6nyg6vst0.png "")
+![](https://community.osarch.org/uploads/editor/fm/zogcrzsmfe4y.png "")
+![](https://community.osarch.org/uploads/editor/qx/zdqum123doe4.png "")
+
+## Feedback 
+Get help: [FreeCAD Trails Workbench (Transportation and Geomatics)](https://forum.freecadweb.org/viewtopic.php?f=8&t=34371)
+
+## Developer 
+Hakan Seven ([@HakanSeven12](https://github.com/HakanSeven12)) and Joel Graff ([@joelgraff](https://github.com/joelgraff)) with inspiration and help from the FreeCAD community
